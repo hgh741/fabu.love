c34692df973860157ff4bea29ad208cdff03ab11
huangguanhui <cy-huanggguanhui@dfl.com.cn>
2020-05-25 19:32:07 +0800

    修改前端页面

--- client/src/components/appDetail/appPreView.vue
+++ client/src/components/appDetail/appPreView.vue
@@ -23,7 +23,7 @@
             <img class="appicon" :src="getIconUrl()" alt="">
             <p class="title">{{this.appBaseData.appName}}</p>
             <div class="info">
-              <p v-if="this.appVersionInfo.versionStr" class="desc">版本：{{this.appVersionInfo.versionStr}}</p><span>大小：{{(this.appVersionInfo.size/1024/1024).toFixed(1)}}</span>
+              <p v-if="this.appVersionInfo.versionStr" class="desc">版本：{{this.appVersionInfo.versionStr}}</p><span>大小：{{(this.appVersionInfo.size/1024/1024).toFixed(1)}}M</span>
             </div>
             <div class="info">
               <p class="desc">发布日期：{{this.appVersionInfo.creatDateStr}}</p><span>更新日志：{{this.appVersionInfo.changelog}}</span>
